6f3af813e08a37f59ffdedc5f5e0040a3062d77d
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-06-26 22:26:44 +0100

    [feat] /coffee returns correct object

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,12 @@ export const app = express();
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
 
 app.get('/coffeelover', (req, res) => res.send('I like coffee'));
+
+app.get('/coffee', (req, res) => {
+  const name: string = typeof req.query?.coffeeName === 'string' ? req.query.coffeeName : 'Latte';
+
+  res.status(200).send({
+    drinkType: 'Coffee',
+    name
+  });
+});
