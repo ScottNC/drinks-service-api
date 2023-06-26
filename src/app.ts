98a3505bd69199921a391970d24da61e9a227a65
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-06-26 22:38:31 +0100

    [feat] express returns json

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
 import express from 'express';
 
 export const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
 
 app.get('/coffeelover', (req, res) => res.send('I like coffee'));
 
-app.get('/coffee', (req, res) => {
-  const name: string = typeof req.query?.coffeeName === 'string' ? req.query.coffeeName : 'Latte';
-
-  res.status(200).send({
-    drinkType: 'Coffee',
-    name
-  });
-});
+app.get('/coffee', (req, res) => res.json({
+  drinkType: 'Coffee',
+  name: 'Latte',
+}));
