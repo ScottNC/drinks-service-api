022e81a4ba5b2b33dc4deb49a64f273579aa4b58
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-06-29 14:13:44 +0100

    [test] includes more tests for /coffee

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -13,3 +13,28 @@ test('GET /coffee should return correct object', async () => {
     name: 'Latte',
   });
 });
+
+test('GET /coffee should return correct object with different coffee nane', async () => {
+  const res = await request(app)
+    .get('/coffee')
+    .query({ coffeeName: 'Phil' });
+
+  expect(res.statusCode).toEqual(200);
+
+  expect(res.body).toEqual({
+    drinkType: 'Coffee',
+    name: 'Phil',
+  });
+});
+
+test('GET /coffee should return correct object with no coffee nane', async () => {
+  const res = await request(app)
+    .get('/coffee');
+
+  expect(res.statusCode).toEqual(200);
+
+  expect(res.body).toEqual({
+    drinkType: 'Coffee',
+    name: 'Latte',
+  });
+});
